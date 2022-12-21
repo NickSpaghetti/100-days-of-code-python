35eb79d8392a060dfdc3a91aef5d69e73a0118bc
Nick Spaghetti <nc_cicchetti@yahoo.com>
2022-12-21 01:47:27 +0000

    added lessons for day two inputs

---
+++ Day_Two/main.py
@@ -0,0 +1,8 @@
+name = input("What is your name?: ")
+favorite_food = input("What is your favorite food?: ")
+favorite_music_genere = input("What is your favorite genre of music?: ")
+
+print(f"""So your name is {0} and you like to eat {1}.
+Oh thats like you really like {2} music""")
+
+print("Thats really neat!")
