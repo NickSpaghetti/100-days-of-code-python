087013a1973de7f22eb3080c7340e61c0e8f45f8
NickSpaghetti <nc_cicchetti@yahoo.com>
2022-12-23 14:16:39 +0000

    Completed Day 5 Challange

---
+++ Day_5/main.py
@@ -0,0 +1,16 @@
+print("Welcome to the world of My Hero Acedemia")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("All might will be asking you a series of Yes or No questions")
+
+is_deku = input("Do you find your self wishing to be strong and overly sensitive? Yes or No")
+if(is_deku == "Yes"):
+  print("Ahh you are like the mighty Deku")
+else:
+  print("You are nothing like Deku")
+
+is_froppy = input("Can you spit out your Stomach")
+if(is_froppy == "Yes"):
+  print("Ewww gro.... I mean neat you are like fropy!")
+else:
+  print("I see you are normal")
+
